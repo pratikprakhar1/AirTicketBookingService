11d0c2a08f4188f681821ffb923c926d665b38e1
Pratik Prakhar <pratikprakhar@Pratiks-MacBook-Air.local>
2023-09-20 21:49:39 +0530

    Completed Setup Message queue b/w Booking and reminder Service

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -15,13 +15,23 @@ class BookingController {
   
     async sendMessageToQueue(req, res) {
       const channel = await createChannel();
-      const data = { message: "Success" };
-      publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+      const payload = { 
+        data : {
+          subject : 'This is a notification from Queue',
+          content : 'Some queue will subscribe this',
+          recepientEmail : 'pprakhar507@gmail.com',
+          notificationTime : '2023-09-20 03:08:3'
+  
+        },
+        service : 'CREATE_TICKET'
+  
+     };
+      publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
       return res.status(StatusCodes.OK).json({
         message: 'Successfully completed booking',
         success: true,
         err: {},
-        data: data
+        data: payload
     })
     } catch (error) {
         return res.status(error.statusCode).json({
